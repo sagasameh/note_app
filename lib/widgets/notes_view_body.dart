2c0d337d84200c91c7facfb301c852c994cd7a99
Your Name <you@example.com>
2023-05-25 03:09:28 +0300

    Create notes'listview

---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_note_item.dart';
+import 'package:note_app/widgets/notes_list_view.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          Expanded(child: NotesListView()),
+        ],
+      ),
+    );
+  }
+}
