7ec9d6b0b384657847176e9eeac66bf7acdafed8
Your Name <you@example.com>
2023-05-25 03:28:25 +0300

    show modal bottom sheet

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/add_note_botttom_sheet.dart';
+import 'package:note_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
+        child: Icon(Icons.add),
+      ),
+      body: const NotesViewBody(),
+    );
   }
 }
