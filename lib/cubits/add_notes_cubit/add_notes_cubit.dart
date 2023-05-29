3ce326d458f10bf48d908902e32c0662b3e54e7f
Your Name <you@example.com>
2023-05-29 14:21:32 +0300

    Provide add note cubit

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/models/note_model.dart';
 
 part 'add_notes_state.dart';
 
-class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(NotesInitial());
+class AddNotesCubit extends Cubit<AddNotesState> {
+  AddNotesCubit() : super(NotesInitial());
+  addNote(NoteModel note) async {
+    emit(AddNotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNotesSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      AddNotesFailure(e.toString());
+    }
+  }
 }
--- lib/cubits/add_notes_cubit/add_notes_state.dart
+++ lib/cubits/add_notes_cubit/add_notes_state.dart
@@ -1,15 +1,15 @@
 part of 'add_notes_cubit.dart';
 
 @immutable
-abstract class NotesState {}
+abstract class AddNotesState {}
 
-class NotesInitial extends NotesState {}
+class NotesInitial extends AddNotesState {}
 
-class AddNotesLoading extends NotesState {}
+class AddNotesLoading extends AddNotesState {}
 
-class AddNotesSuccess extends NotesState {}
+class AddNotesSuccess extends AddNotesState {}
 
-class AddNotesFailure extends NotesState {
+class AddNotesFailure extends AddNotesState {
   final String errorMessage;
 
   AddNotesFailure(this.errorMessage);
