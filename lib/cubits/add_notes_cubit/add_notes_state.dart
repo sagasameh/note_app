f6929e1ac614ae5dfd6d6e2d3129a7c6be91cb80
Your Name <you@example.com>
2023-05-29 13:52:27 +0300

    Add add notes states

--- lib/cubits/add_notes_cubit/add_notes_state.dart
+++ lib/cubits/add_notes_cubit/add_notes_state.dart
@@ -4,3 +4,13 @@ part of 'add_notes_cubit.dart';
 abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
+
+class AddNotesLoading extends NotesState {}
+
+class AddNotesSuccess extends NotesState {}
+
+class AddNotesFailure extends NotesState {
+  final String errorMessage;
+
+  AddNotesFailure(this.errorMessage);
+}
